52cd8ad82676c918519fed302f2c80f68cafadbe
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2025-07-27 01:22:15 +0530

    taiwind config file corrected

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import Experience from "../components/Experience";
 import Projects from "../components/Projects";
 import Education from "../components/Education";
 import Contact from "../components/Contact";
+import "../index.css"
 
 const Index = () => {
   return (
